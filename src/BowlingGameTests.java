0bd4da9f291e6b6c3adf1f2002736e4a949b2d68
255671 <255671@edu.p.lodz.pl>
2026-05-30 12:21:46 +0200

    Dodano test najgorszej gry

---
+++ src/BowlingGameTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BowlingGameTests {
+    private Game game;
+
+    @BeforeEach
+    protected void initialize(){
+        game = new Game();
+    }
+
+    @Test
+    void testWorstGame(){
+        rollMany(20,0);
+        assertEquals(0, game.score());
+    }
+
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            game.roll(pins);
+        }
+    }
+}
